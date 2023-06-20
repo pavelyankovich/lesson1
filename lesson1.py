3327c1dc37d941d88291276a6fc35ada4a8fe710
Teachers PC <top@academy.purple_lab>
2023-06-20 21:17:09 +0300

    первый раз в первый класс

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+name = input('Как твое имя? ')
+age = int(input('Сколько лет? '))
+
+if age<=30:
+    print(f'Привет, {name}!')
+else:
+    print(f'Вы кто такой, я Вас не звал,Идите - {name} - ***')
+
+print('the end')
+
